b1b6f32d7b262fac7423504f30d025d85eba0160
luckenco <cluckenbach@protonmail.com>
2025-01-27 14:00:36 +0100

    add deepseek provider client
    For now this won't have pooled connections. We make it work, then make
    it fancy.

--- src/deepseek.rs
+++ src/deepseek.rs
@@ -1,16 +1,18 @@
-use model::REASONER;
+use reqwest::{header, Client as HttpClient};
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
+use std::time::Duration;
 
 pub mod completions;
 pub mod request;
 
-pub mod model {
-    pub const CHAT: &'static str = "deepseek-chat";
-    pub const REASONER: &'static str = "deepseek-reasoner";
-}
+pub const MODEL: &'static str = "deepseek-reasoner";
 
 pub struct Config {
     pub base_url: &'static str,
     pub model: &'static str,
+    pub connection_timeout: Duration,
+    pub max_retries: u32,
 }
 
 impl Config {
@@ -23,22 +25,41 @@ impl Default for Config {
     fn default() -> Self {
         Self {
             base_url: "https://api.deepseek.com",
-            model: REASONER,
+            model: MODEL,
+            max_retries: 3,
+            // Keep in mind that this should be way lower when streaming completion chunks.
+            connection_timeout: Duration::from_secs(30),
         }
     }
 }
 
-// TODO: response_format
-// When using JSON Output, you must also instruct the model to produce JSON yourself via a system or user message.
-// So we we will check the messages Vec if this is included? or is this to intrusive?
+pub struct Client {
+    pub inner: ClientWithMiddleware,
+}
 
-// TODO: stream
-// SSE terminated by data:[DONE]
+impl Client {
+    pub fn new(api_key: &str, config: Config) -> Self {
+        let mut headers = header::HeaderMap::new();
+        headers.insert("content-type", "application/json".parse().unwrap());
+        headers.insert(
+            "authorization",
+            format!("Bearer {}", api_key).parse().unwrap(),
+        );
 
-// TODO: tools
-// Introduce function calling
+        let http_client = HttpClient::builder()
+            .timeout(config.connection_timeout)
+            .default_headers(headers)
+            .build()
+            .expect("Failed to create HTTP client");
 
-// TODO: tool_choice
-// Use serde annotations on the enum or impl From<_> ?
+        let retry_policy = ExponentialBackoff::builder().build_with_max_retries(config.max_retries);
 
-// TODO: logprobs
+        let retry_client = ClientBuilder::new(http_client)
+            .with(RetryTransientMiddleware::new_with_policy(retry_policy))
+            .build();
+
+        Self {
+            inner: retry_client,
+        }
+    }
+}
